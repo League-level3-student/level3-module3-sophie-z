ec5618bcaa75b02c2e3d16c66defbda925926525
sophie-z <sophie.zeng2@gmail.com>
2021-05-18 20:01:09 -0700

    .

--- src/_00_Text_Funkifier/BackwardsString.java
+++ src/_00_Text_Funkifier/BackwardsString.java
@@ -8,8 +8,12 @@ public class BackwardsString extends SpecialString {
 
 	@Override
 	public String funkifyText(String s) {
-		
-		return "";
+		String reversed = "";
+		for(int i = s.length() - 1; i>=0; i--) {
+			reversed = reversed + s.charAt(i);
+		}
+		s = reversed;
+		return s;
 	}
 
 }
--- src/_00_Text_Funkifier/MixedCapsString.java
+++ src/_00_Text_Funkifier/MixedCapsString.java
@@ -8,8 +8,19 @@ public class MixedCapsString extends SpecialString{
 
 	@Override
 	public String funkifyText(String s) {
-		
-		return "";
+		String funkified = "";
+		for(int i = 0; i<s.length(); i++) {
+			if(s.charAt(i) == ' ') {
+				
+			}
+			if(i%2 == 1) {
+				funkified += Character.toUpperCase(s.charAt(i));
+			}
+			else if(i%2 == 0 || i ==0) {
+				funkified += Character.toLowerCase(s.charAt(i));
+			}
+		}
+		return funkified;
 	}
 
 }

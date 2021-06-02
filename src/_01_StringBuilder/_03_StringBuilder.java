3f1a25b17aad6ec1f1fe48263cd16b138c6ed7bc
sophie-z <sophie.zeng2@gmail.com>
2021-06-01 19:32:49 -0700

    .

--- src/_01_StringBuilder/_03_StringBuilder.java
+++ src/_01_StringBuilder/_03_StringBuilder.java
@@ -4,18 +4,30 @@ package _01_StringBuilder;
 public class _03_StringBuilder {
     
     public static String append(String str, char[] characters) {
-        return null;
+    	StringBuilder sb = new StringBuilder(str);
+    	for(int i = 0; i<characters.length; i++) {
+    		str = sb.append(characters[i]).toString();
+    		System.out.println(str);
+    	}
+        return str;
     }
     
     public static String reverse(String str) {
-        return null;
+    	StringBuilder sb = new StringBuilder(str);
+    	str = sb.reverse().toString();
+    	System.out.println(str);
+    	return str;
     }
     
     public static String insert(String str, int index, char newChar) {
-        return null;
+    	StringBuilder sb = new StringBuilder(str);
+    	str = sb.insert(index, newChar).toString();
+    	return str;
     }
     
     public static String delete(String str, int startIndex, int endIndex) {
-        return null;
+    	StringBuilder sb = new StringBuilder(str);
+    	str = sb.delete(startIndex, endIndex).toString();
+    	return str;
     }
 }
